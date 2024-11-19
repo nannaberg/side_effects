0e38dc90924e8c44f1c43ce00ae4126adca7a1c1
Nanna Berg <bergnm@hotmail.com>
2024-11-19 22:53:42 +0100

    more cleanup

--- july_version/final_promed.py
+++ july_version/final_promed.py
@@ -6,7 +6,6 @@ import re
 import lxml
 import html2text
 from html_sanitizer import Sanitizer
-from collections import Counter
 import pandas as pd
 from datetime import datetime
 from io import StringIO
@@ -97,6 +96,7 @@ def check_unmerged_table(modified_rows, table, index, atc):
             actual = modified_rows[i][j]
             if column != "Advarsel":
                 if expected != actual:
+                    print(expected, actual)
                     if not both_are_na(expected, actual):
                         raise Exception(
                             "A table test failed: {}, {}".format(index, atc)
@@ -192,23 +192,18 @@ def get_renal_info(soup, index, atc):
 
                 final_res += res + "\n\n"
 
-            # adding potential text after all the tables
+            # adding text, if any, after all the tables
             for table in tables:
                 table.extract()
             text_without_tables = get_sanitized_html_markdown_text(div).rstrip("\n")
             final_res += text_without_tables
-            # print("FINAL RES")
-            # print(final_res, index, atc)
 
     else:
-        # has_no_tables.append(index)
         final_res = get_sanitized_html_markdown_text(div)
     return final_res
 
 
 def get_liver_info(soup, atc_code, index, text_header):
-
-    # header = soup.find(lambda tag: tag.name == "h3" and text_header in tag.text)
     header = get_header(soup, text_header)
 
     res = "na"
@@ -237,7 +232,6 @@ def get_halftime(soup, atc, index, text_header):
 
     if halftime_lines:
         res = "\n".join(halftime_lines)
-    # print("{}, {} \n{}".format(index, atc, res))
     return res
 
 
@@ -256,19 +250,9 @@ def get_revision_date(soup, atc, index):
 
 
 def remove_commas_between_bold(index, atc, markdown_text):
-    # annoying_commas = ["** ,**", "** , **", "**, **"]
-    # altered_markdown = markdown_text
-    # for comma in annoying_commas:
-    #     if comma in markdown_text:
-    #         altered_markdown = altered_markdown.replace(comma, ",")
-    #         print("ANNOYING COMMA WAS HERE: ", index, atc)
-    # return altered_markdown
     comma_pattern = r"\*\*\s*,\s*\*\*"
     altered_markdown = markdown_text
     if re.search(comma_pattern, markdown_text):
-        print("BOLD COMMA WAS HERE: ", index, atc)
-        print(markdown_text)
-        print("-----------------------")
         altered_markdown = re.sub(comma_pattern, ",", markdown_text)
     return altered_markdown
 
@@ -279,31 +263,18 @@ def remove_annoying_punctuation_from_non_bold(index, atc, non_bold):
         "**.**",
         "_._",
     ]
-    match = False
     res = []
     for nb in non_bold:
         new_nb = nb
         for period in annoying_periods:
             if period in new_nb:
                 new_nb = new_nb.replace(period, ".")
-                match = True
-                # print("ANNOYING PERIOD: ", atc, index)
-                # print("nb: \n", nb)
-                # print("new nb: \n", new_nb)
-                # print("--------------")
         if new_nb[0] == ".":
-            match = True
-            # print(new_nb)
             new_nb = new_nb[1:].strip()
-            # print(new_nb)
         if new_nb[-1] == "*" and new_nb[-2:] != "**":
             match = True
             new_nb = new_nb[:-1].strip()
         res.append(new_nb)
-        # if match:
-        #     print(nb)
-        #     print(new_nb)
-        #     print("---------------")
     return res
 
 
@@ -324,7 +295,7 @@ def get_all_bold(atc, index, markdown_text, pattern, exclusion_list):
     contains_colored_tablets = False
     for r in res:
         if "tabletter" in r:
-            # to check for the 21 lyserøde tabletter, 21 tabletter cases
+            # to check for the 21 lyserøde tabletter, 21 tabletter, etc cases
             if r[0].isdigit():
                 contains_colored_tablets = True
                 break
@@ -419,13 +390,11 @@ def get_pharmaceutical_form(soup, atc, index):
     else:
         for bold, non_bold in zip(pharma_forms, pharma_non_bold):
             if any(s in non_bold for s in ["filmovertruk", "flmovertruk"]):
-                # print("{}, {}, {}: {}".format(index, atc, bold, non_bold))
                 new_pharma_forms.append(bold + ", filmovertrukne")
             elif "sukkerovertruk" in non_bold:
                 new_pharma_forms.append(bold + ", sukkerovertrukne")
             elif "overtruk" in non_bold:
                 new_pharma_forms.append(bold + ", overtrukne")
-                # print("{}, {}, {}: {}".format(index, atc, bold, non_bold))
             else:
                 new_pharma_forms.append(bold)
     pharma_non_bold = remove_annoying_punctuation_from_non_bold(
@@ -506,7 +475,6 @@ async def get(
 def expand_pharma_forms(input):
     forms_key = "Pharmaceutical form"
     amount_key = "Pharmaceutical form dosage"
-    print(forms_key, amount_key)
     df = pd.DataFrame.from_dict(input)
     df = df.explode([forms_key, amount_key], ignore_index=True)
     return df
@@ -531,19 +499,14 @@ async def main(data, data_not_on_promed):
     async with aiohttp.ClientSession(timeout=timeout) as session:
         scraping_res = await asyncio.gather(*[get(session, d) for d in data])
         only_res = [r for r in scraping_res if r]
-
-        print("original res: ", len(only_res))
         pharma_expanded_df = expand_pharma_forms(only_res)
-        print("expanded res: ", pharma_expanded_df.shape[0])
         pharma_duplicate_free_df = remove_pharma_duplicates(pharma_expanded_df)
-        print("duplicate free res: ", pharma_duplicate_free_df.shape[0])
         dst = "working_example.csv"
 
         # removing pharmaceutical form dosage from final results, they were only used for getting correct pharmaceutical form
         final_pharma_df = pharma_duplicate_free_df.loc[
             :, pharma_duplicate_free_df.columns != "Pharmaceutical form dosage"
         ]
-        print(final_pharma_df.columns)
 
         # adding empty entries for the atc codes not found on promed:
         for d in data_not_on_promed:
@@ -558,35 +521,15 @@ async def main(data, data_not_on_promed):
             ["Atc", "Index"], ascending=[True, True]
         )
         final_pharma_df.to_csv(dst, index=False, escapechar=None)
+        print("All done: Number of rows in final result: ", final_pharma_df.shape[0])
 
 
 if __name__ == "__main__":
     data = get_all_data_with_indices()
     data_not_on_promed = get_data_not_on_promed()
-    print("Atc's NOT ON PROMED: ", data_not_on_promed)
-    # print(len(data))
-    # print(*data[:10], sep="\n")
-
-    to_skip = ["A01AB09", "A06AD15", "M02AA07", "A10AC04", "A10AE54", "A10BD10"]
-    to_keep_empty = ["A06AG02", "N02CX02"]
-
     start = perf_counter()
-    # print(data[0])
-    indices = ["4104", "5988", "5965", "9989"]
-    indices = ["3380"]
-    indices = ["10675"]
-    indices = ["5988", "5965", "10675"]
-    indices = ["2765"]
-    indices = ["521"]
-    indices = ["4651"]
     # indices = ["2605", "8316", "9517"]
     # data_used = [d for d in data if d[5] in indices[0]]
-    # print(data_used)
-    data_used = data[:100]
-    # data_used = [d for d in data if d[4] == "5965"]
-    # print(data_used)
+    data_used = data
     asyncio.run(main(data_used, data_not_on_promed))
     end = perf_counter()
-
-    # print(f"{len(data) / (end - start)} req/s")
-    # print("total time: ", end - start)
