508f1d324a36a8801dc941442a6657434515d1ae
Nanna Berg <bergnm@hotmail.com>
2024-11-19 20:41:40 +0100

    unmerged tables are checked

--- july_version/final_promed.py
+++ july_version/final_promed.py
@@ -9,6 +9,7 @@ from html_sanitizer import Sanitizer
 from collections import Counter
 import pandas as pd
 from datetime import datetime
+from io import StringIO
 
 pd.set_option("display.max_rows", None)
 pd.set_option("display.max_columns", None)
@@ -79,31 +80,49 @@ def check_no_text_between_tables(outer_tables, index, atc):
                     )
 
 
+def both_are_na(expected, actual):
+    return pd.isna(expected) and actual == "na"
+
+
+def check_unmerged_table(modified_rows, table, index, atc):
+    # testing parts of the unmerged table against a pd parsed table
+    # can't use the pd parsed table instead of own unmerging method, since some warnings are
+    # better represented with markdown
+    first_tr = table.find("tr")
+    first_tr.extract()
+    df = pd.read_html(StringIO(str(table)))[0]
+
+    for i, row in df.iterrows():
+        for j, (column, expected) in enumerate(row.items()):
+            actual = modified_rows[i][j]
+            if column != "Advarsel":
+                if expected != actual:
+                    print(index, atc, expected, actual)
+                    if not both_are_na(expected, actual):
+                        raise Exception(
+                            "A table test failed: {}, {}".format(index, atc)
+                        )
+            else:
+                if not both_are_na and len(expected) < len(actual):
+                    raise Exception("A table test failed: {}, {}".format(index, atc))
+
+
 def get_renal_info(soup, index, atc):
     final_res = "na"
     text_header = "Nedsat nyrefunktion"
     header = get_header(soup, text_header)
     if not header:
         return final_res
-
     div = header.parent.find_next_sibling("div")
-
-    # has_nested_tables = []
-    # has_regular_tables = []
-    # has_no_tables = []
     tables = div.find_all("table")
 
     outer_tables = []
     first_table = div.find("table")
     if first_table:
-        # print(index, atc)
         outer_tables = [first_table] + first_table.find_next_siblings("table")
         if len(outer_tables) != len(tables):
-            # has_nested_tables.append(index)
             final_res = "Nested table - to be done manually"
         else:
-            # has_regular_tables.append(index)
-            # print_this = False
             final_res = ""
             check_no_text_between_tables(outer_tables, index, atc)
             for table in outer_tables:
@@ -115,10 +134,8 @@ def get_renal_info(soup, index, atc):
                             text_header, index, atc
                         )
                     )
-                # general_header_soup = table.find("tr", class_="Header")
                 general_header_text = get_sanitized_html_markdown_text(all_headers[0])
                 headers_soup = all_headers[1]
-                # headers_soup = general_header_soup.find_next_sibling("tr")
                 headers = [header.get_text() for header in headers_soup.find_all("th")]
 
                 content_soup = headers_soup.find_next_siblings("tr")
@@ -128,6 +145,7 @@ def get_renal_info(soup, index, atc):
                 ]
                 res = general_header_text
 
+                # unmerging merged cells in the html table
                 skip_cells = {}
                 modified_rows = []
                 for i, row in enumerate(rows):
@@ -154,7 +172,9 @@ def get_renal_info(soup, index, atc):
                             new_text = skip_cells[i, j]
                             new_modified_row.insert(j, new_text)
                     new_modified_rows.append(new_modified_row)
+                check_unmerged_table(new_modified_rows, table, index, atc)
 
+                # building the string representing the table from the unmerged table
                 indent = 4 * " "
                 for k, row in enumerate(new_modified_rows):
                     res_line = indent
@@ -173,6 +193,7 @@ def get_renal_info(soup, index, atc):
 
                 final_res += res + "\n\n"
 
+            # adding potential text after all the tables
             for table in tables:
                 table.extract()
             text_without_tables = get_sanitized_html_markdown_text(div).rstrip("\n")
